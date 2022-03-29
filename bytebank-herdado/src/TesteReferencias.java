db5973f28642f84710af3e8207c82e0fba11cbfc
Luís Roberto <luisldjsilva@gmail.com>
2022-03-28 22:40:24 -0300

    Criando lista de exercicios do curso de Java da Plataforma Alura

---
+++ bytebank-herdado/src/TesteReferencias.java
@@ -0,0 +1,38 @@
+
+public class TesteReferencias {
+	
+	public static void main(String[] args) {
+		
+		Gerente g1 = new Gerente();
+		g1.setNome("Marcos");
+		g1.setSalario(5000);
+		String nome = g1.getNome();
+		
+		Funcionario f1 = new Funcionario();
+		f1.setSalario(2000);
+		
+		EditorVideo ev = new EditorVideo();
+		f1.setSalario(2500);
+		
+		Designer d = new Designer();
+		d.setSalario(2000);
+		
+		
+		ControleBonificacao controle = new ControleBonificacao();
+		controle.registra(g1);
+		controle.registra(f1);
+		controle.registra(ev);
+		controle.registra(d);
+
+		
+		
+		
+		System.out.println();
+		
+		System.out.println(nome);
+		
+		System.out.println(controle.getSoma());
+		
+	}
+
+}
